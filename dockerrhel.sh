f4c75ce02ee60229ffb07dcbdb69bb9f3d52e1d4
Emmanual4 <emmanual4@gmail.com>
2018-11-18 15:52:55 +0530

    Create dockerrhel.sh

---
+++ dockerrhel.sh
@@ -0,0 +1,12 @@
+# install docker
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum makecache fast
+sudo yum install --setopt=obsoletes=0 \
+   docker-ce-17.03.2.ce-1.el7.centos.x86_64 \
+   docker-ce-selinux-17.03.2.ce-1.el7.centos.noarch
+sudo systemctl start docker
+
+# install docker-compose
+sudo yum install -y python-pip
+sudo pip install docker-compose
